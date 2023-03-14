8d924157fb136828613ff61358993125faaa4026
Connor Murray <55203772+connor122721@users.noreply.github.com>
2023-03-14 17:26:15 -0400

    Scripts for variant age estimates

---
+++ 1.Run_GEVA.R
@@ -0,0 +1,138 @@
+# Trans species polymorphism - trees
+# 2.28.2023
+# module load goolf/7.1.0_3.1.4 R/4.0.3; module load gdal geos proj; R
+
+# Libraries
+library(data.table)
+library(tidyverse)
+library(cowplot)
+library(viridis)
+library(patchwork)
+
+# Working directory
+setwd("/project/berglandlab/connor/")
+
+# SNP metadata
+tot <- readRDS(file="/project/berglandlab/connor/data/classified_snps_filt.rds") %>% 
+  mutate(chrom=as.integer(tstrsplit(chrom, "_")[[2]]))
+
+# Betascan results
+beta <- readRDS(file="/project/berglandlab/connor/data/betascan.daphnia.TSP.rds") %>% 
+  mutate(chrom=as.integer(tstrsplit(chrom, "_")[[2]]))
+
+# Save output
+ds <- data.table(rbind(fread("for_daniel/nam.samps.TMRCA.txt"),
+                 fread("for_daniel/euro.samps.TMRCA.txt")))
+
+
+names(ds) = c("chrom","position","ID","ClockModel","Filter","N_Concordant",
+              "N_Discordant", "PMean","PMode","PMedian","Set")
+
+# Metadata
+ds1 <- na.omit(data.table(ds %>% 
+                left_join(tot, by=c("chrom", "position"))) %>% 
+  mutate(classified1=case_when(classified=="shared_poly"~"TSP",
+                               TRUE ~ "Not TSP")))
+
+# Gene-wise average
+ds2 <- data.table(ds1 %>% 
+            group_by(classified1, gene, Set) %>% 
+            summarize(mean=mean(PMean, na.rm = T),
+                      sd=sd(PMean, na.rm = T),
+                      n=n()) %>% 
+            mutate(se=sd/sqrt(n)))
+
+ds2 %>% 
+  ggplot(.) +
+  geom_pointrange(aes(x=classified1, 
+                      y=mean, 
+                      ymin=mean-2*se, 
+                      ymax=mean+2*se, 
+                      color=classified1)) +
+  facet_wrap(~Set, nrow = 1)
+
+# Plot cpd_wb  distance output
+plot <- {
+  
+  ds1 %>% 
+  ggplot(., 
+         aes(x=simpleAnnot, 
+             y=PMean,
+             fill=classified1)) +
+  geom_boxplot() +
+  scale_y_log10() +
+  coord_flip() +
+  facet_wrap(~Set, nrow=1) +
+  scale_fill_manual(values = c("TSP"="darkcyan",
+                               "Not TSP"="gray")) +
+  theme_bw() +
+  labs(x = "SNP classification", 
+       fill = "",
+       y = "Mean TMRCA") +
+  theme(legend.title = element_text(face="bold", size=18), 
+        strip.text = element_text(face="bold", size=18), 
+        axis.text.x = element_text(face="bold", size=18),
+        axis.text.y = element_text(face="bold", size=18),
+        axis.title.x = element_text(face="bold", size=18),
+        axis.title.y = element_text(face="bold", size=18),
+        axis.title = element_text(face="bold", size=20))
+  
+}
+
+ggsave("candgene/geva.euro.nam.tmrca.pdf", plot)
+
+# Stats
+t.test(ds1[classified1=="TSP"][simpleAnnot=="NS"][!Set %like% "nam"]$PMean,
+       ds1[classified1=="Not TSP"][simpleAnnot=="NS"][!Set %like% "nam"]$PMean)
+
+t.test(ds1[classified1=="TSP"][simpleAnnot=="NS"][!Set %like% "nam"]$PMean,
+       ds1[classified1=="Not TSP"][simpleAnnot=="NS"][!Set %like% "nam"]$PMean)
+
+# Add intervals
+upp=10
+dt2 <- data.table(ds1[gene.x==gene.y] %>% 
+                    #group_by(.id, SNP_A) %>% 
+                    mutate(distc = as.factor(cut(dist, 
+                                                 breaks = seq(from = 0,
+                                                              to = max(dist) + upp,
+                                                              by = upp), right = F))))
+
+# Add Gene x Gene comps
+dt3 <- data.table(dt2 %>% 
+                    mutate(gene_comp = case_when(classified.x=="shared_poly" & 
+                                                   classified.y=="shared_poly" ~ "TSP",
+                                                 TRUE ~ "Not TSP")))
+
+# Summarize
+dt.fin <- data.table(dt3 %>% 
+                       group_by(dist, cont, gene_comp) %>% 
+                       summarise(mean = mean(R2, na.rm = T),
+                                 uci.r2 = quantile(R2, probs = 0.95, na.rm = T),
+                                 lci.r2 = quantile(R2, probs = 0.05, na.rm = T),
+                                 median = median(R2, na.rm = T)))
+
+
+{
+  
+  ds1 %>% 
+    ggplot(., 
+           aes(x=PMean, 
+               y=Beta)) +
+    geom_point() +
+    scale_y_log10() +
+    coord_flip() +
+    scale_fill_manual(values = c("TSP"="darkcyan",
+                                 "Not TSP"="gray")) +
+    theme_bw() +
+    labs(x = "SNP classification", 
+         fill = "",
+         y = "Mean TMRCA") +
+    theme(legend.title = element_text(face="bold", size=18), 
+          strip.text = element_text(face="bold", size=18), 
+          axis.text.x = element_text(face="bold", size=18),
+          axis.text.y = element_text(face="bold", size=18),
+          axis.title.x = element_text(face="bold", size=18),
+          axis.title.y = element_text(face="bold", size=18),
+          axis.title = element_text(face="bold", size=20))
+  
+}
---
+++ Run.GevaEuro.sh
@@ -0,0 +1,141 @@
+#!/usr/bin/env bash
+#SBATCH -J geva_europe # A single job name for the array
+#SBATCH --ntasks-per-node=15 # one core
+#SBATCH -N 1 # on one node
+#SBATCH -t 1-00:00
+#SBATCH --mem 50G
+#SBATCH -o /project/berglandlab/connor/err/geva_europe.%A_%a.out # Standard output
+#SBATCH -e /project/berglandlab/connor/err/geva_europe.%A_%a.err # Standard error
+#SBATCH -p standard
+#SBATCH --account berglandlab
+
+# Load Modules
+module load tabix
+module load bcftools
+module load vcftools
+
+# Link programs
+GEVA=/home/csm6hg/geva
+
+# Link inout VCF
+wd="/project/berglandlab/connor/for_daniel"
+INPUT_VCF=${wd}/daphnia.whatshap.ann.vcf.gz
+
+# Samples
+SAMPLEIDS=${wd}/euro.samps
+GUIDE=${wd}/goodChrom.txt
+rename=${wd}/Scaffold_names.txt
+
+# Performance parameters
+CPU=15
+
+# Biological Parameters
+REC=1.60e-08 #Lynch et al. -- doi: https://doi.org/10.1101/2020.03.03.974485
+MUT=5.69e-09 #Keith et al. -- doi: https://doi.org/10.1101/gr.191338.115
+NE=200000
+
+# Generate Internal Variables
+CHR=$( cat $GUIDE | sed -n ${SLURM_ARRAY_TASK_ID}p )
+chrom=$( cat $rename | sed -n ${SLURM_ARRAY_TASK_ID}p | cut -f2 )
+echo "Now processing CHR:" ${CHR} ${chrom}
+name=${CHR}.euro
+
+# VCFtools portion
+vcftools --gzvcf ${INPUT_VCF} \
+--chr ${CHR} \
+--maf 0.01 \
+--keep ${SAMPLEIDS} \
+--recode --recode-INFO-all \
+--out ${name}
+
+# add annotation
+bcftools query \
+-f '%CHROM\t%POS\t%POS\t%CHROM\_%POS\n' \
+${name}.recode.vcf > ${name}.annotation.txt
+
+#Index the annotation
+bgzip ${name}.annotation.txt
+tabix -s1 -b2 -e2 ${name}.annotation.txt.gz
+
+# Add annotation
+bcftools annotate \
+-a ${name}.annotation.txt.gz \
+-c CHROM,FROM,TO,ID \
+--rename-chrs ${rename} \
+${name}.recode.vcf > ${name}.SNPannotated.forGEVA.vcf
+
+bgzip ${name}.SNPannotated.forGEVA.vcf
+tabix ${name}.SNPannotated.forGEVA.vcf.gz
+
+# Phase
+module load gcc/9.2.0 shapeit4/4.1.3
+
+# Run shapeit
+shapeit4 \
+--input ${name}.SNPannotated.forGEVA.vcf.gz \
+--region ${chrom} \
+-T ${CPU} \
+-O ${name}.phased.vcf
+
+# Make bin files
+${GEVA}/geva_v1beta \
+--vcf ${name}.phased.vcf  \
+--rec ${REC} \
+-t ${CPU} \
+--out ${name}
+
+### Run the TMRCA loop
+for i in `cat ${name}.marker.txt \ |
+sed '1d' |  \
+awk '{print $3}'`
+do
+
+#i=10421608
+echo ${i}
+
+# Run GEVA
+${GEVA}/geva_v1beta \
+--input ${name}.bin \
+--out RUN_${i} \
+--treads ${CPU} \
+--mut ${MUT} \
+--hmm hmm_initial_probs.txt hmm_emission_probs.txt \
+--Ne ${NE} \
+--position ${i}
+
+# Make TEMP file
+TMP=`sed -n '7p' RUN_${i}.sites.txt`
+
+# Conditionally output
+if [ -z "$TMP" ]
+then
+	echo  " RUN_${i} is empty "
+else
+	#Print to common file
+	echo -e \
+	${chrom} \
+	${i} \
+	${TMP} \
+	${SAMPLEIDS} \
+	>> ${SAMPLEIDS}.TMRCA.txt
+fi
+
+# Remove Clutter inside the loop
+rm RUN_${i}.err
+rm RUN_${i}.log
+rm RUN_${i}.pairs.txt
+rm RUN_${i}.sites.txt
+
+# Finish loop
+done
+
+# Remove Clutter outside the loop
+rm ${name}.recode.vcf
+rm ${name}.annotation.txt.gz
+rm ${name}.annotation.txt.gz.tbi
+rm ${name}.log
+rm ${name}.err
+rm ${name}.SNPannotated.forGEVA.vcf
+rm ${name}.bin
+rm ${name}.sample.txt
+rm ${name}.marker.txt
